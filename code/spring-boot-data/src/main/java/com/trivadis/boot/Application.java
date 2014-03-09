2923138ed8aa43ac8e0b16c9ba1d0e36a94f2ace
Raffael Schmid <raffael.schmid@trivadis.com>
2014-03-09 23:11:23 +0100

    add transactional detection

--- code/spring-boot-data/src/main/java/com/trivadis/boot/Application.java
+++ code/spring-boot-data/src/main/java/com/trivadis/boot/Application.java
@@ -3,9 +3,11 @@ package com.trivadis.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @EnableAutoConfiguration
+@EnableTransactionManagement
 @ComponentScan
 public class Application{
 	public static void main(String[] args) throws Exception {
