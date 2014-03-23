30195ab779cdcb6f44ff0983989acc205964b7f4
Raffael Schmid <raffael.schmid@trivadis.com>
2014-03-23 09:52:30 +0100

    -

--- code/web-security/src/main/java/com/trivadis/boot/sec/userdetails/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trivadis.boot.sec.userdetails;
-
-import org.springframework.data.repository.Repository;
-
-public interface UserRepository extends Repository<User, String>{
-
-	User findByName(String username);
-
-}
