030f4576275faac0d7b80a13cc302382da857578
Raffael Schmid <raffael.schmid@trivadis.com>
2014-03-23 07:51:24 +0100

    demo step01

---
+++ demo/city-of-switzerland/src/main/java/com/trivadis/cities/City.java
@@ -0,0 +1,55 @@
+package com.trivadis.cities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class City {
+	
+	@Id
+	private Long zip;
+	
+	private String name;
+
+	public Long getZip() {
+		return zip;
+	}
+
+	public void setZip(Long zip) {
+		this.zip = zip;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((zip == null) ? 0 : zip.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		if (zip == null) {
+			if (other.zip != null)
+				return false;
+		} else if (!zip.equals(other.zip))
+			return false;
+		return true;
+	}
+	
+}
